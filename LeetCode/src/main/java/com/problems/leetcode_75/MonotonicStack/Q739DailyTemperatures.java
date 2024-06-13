9979c6e27bf3442f50f1e9d5487f9fe00519d981
Q Chen <qche.au@gmail.com>
2024-06-13 19:28:16 +1000

    add solution for 739.Daily Temperatures

---
+++ LeetCode/src/main/java/com/problems/leetcode_75/MonotonicStack/Q739DailyTemperatures.java
@@ -0,0 +1,54 @@
+package com.problems.leetcode_75.MonotonicStack;
+
+import java.util.Arrays;
+
+public class Q739DailyTemperatures {
+
+    // https://leetcode.com/problems/daily-temperatures/?envType=study-plan-v2&envId=leetcode-75
+    public static void main(String[] args) {
+        final String pass = "PASSED", fail = "FAILED";
+        Solution sol = new Solution();
+
+        // Example 1
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] expected = {1, 1, 4, 2, 1, 1, 0, 0};
+        int[] actual = sol.dailyTemperatures(temperatures);
+        String result = Arrays.equals(expected, actual) ? pass : fail;
+        System.out.printf("The actual is %s, the expected is %s, the result has %s.\n", Arrays.toString(actual), Arrays.toString(expected), result);
+
+        // Example 2
+        temperatures = new int[]{30, 40, 50, 60};
+        expected = new int[]{1, 1, 1, 0};
+        actual = sol.dailyTemperatures(temperatures);
+        result = Arrays.equals(expected, actual) ? pass : fail;
+        System.out.printf("The actual is %s, the expected is %s, the result has %s.\n", Arrays.toString(actual), Arrays.toString(expected), result);
+
+        // Example 3
+        temperatures = new int[]{30, 60, 90};
+        expected = new int[]{1, 1, 0};
+        actual = sol.dailyTemperatures(temperatures);
+        result = Arrays.equals(expected, actual) ? pass : fail;
+        System.out.printf("The actual is %s, the expected is %s, the result has %s.\n", Arrays.toString(actual), Arrays.toString(expected), result);
+    }
+
+    private static class Solution {
+        public int[] dailyTemperatures(int[] t) {
+            int n = t.length;
+            int[] ans = new int[n];
+            int[] stack = new int[n];
+
+            int j = -1;
+            int i = 0;
+            while (i < n) {
+                while (j >= 0 && t[stack[j]] < t[i]) {
+                    ans[stack[j]] = i - stack[j];
+                    --j;
+                }
+                stack[++j] = i;
+                ++i;
+            }
+            return ans;
+        }
+    }
+
+}
